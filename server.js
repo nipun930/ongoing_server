96ba4498c3ce426527d53a7cce6f397c89ea18b4
Nipun Kumar <nipunkumar930@gmail.com>
2020-04-26 10:07:54 +0530

    setting port of app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dbconfig = require('./app/config/configuration');
 
 const app = express();
 const ngrok = require('ngrok');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 mongoose.connect(dbconfig.url,{useUnifiedTopology: true, useNewUrlParser: true}).then(()=>{
     console.log('connected to mongodb');
-    app.listen( port,'13.59.181.119', () => {
+    app.listen( port, () => {
         index(app);
         app.get('/test', (req, res)=>{
             res.json("server is working now");
